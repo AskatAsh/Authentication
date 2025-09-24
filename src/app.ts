0822f4d25d37e078dca87ab0ce3e2d28e1c5c759
AskatAsh <abdulaskat@gmail.com>
2025-09-25 01:11:14 +0600

    used middleware express.json and cors with user routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
+import cors from 'cors';
 import express, { Request, Response } from "express";
+import { UserRoutes } from "./app/modules/user/user.route";
+
+// create express app
 const app = express();
 
+// middleware to parse request body
+app.use(express.json());
+
+// middleware for cross origin resource sharing
+app.use(cors());
+
+// middleware for user routes
+app.use('/api/v1/user', UserRoutes);
+
+// root get api
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
         message: "Authentication server is running..."
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,18 +3,18 @@ import { Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import User from "./user.model";
 
-const createUser = (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response) => {
     try {
         const { name, email } = req.body;
 
-        const user = User.create({
+        const user = await User.create({
             name, email
         })
 
         res.status(httpStatus.CREATED).json({
             success: true,
             message: "User created successfully!",
-            user
+            data: user
         })
 
     } catch (error: any) {
