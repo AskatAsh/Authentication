764123598a782e39f953d749062ba0560b9b29bf
AskatAsh <abdulaskat@gmail.com>
2025-09-26 21:34:44 +0600

    created HOC function to avoid repetition of try catch

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,40 +1,48 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
 import { UserServices } from "./user.service";
 
-// controller to create user
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = await UserServices.createUser(req.body);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserServices.createUser(req.body);
 
-        res.status(httpStatus.CREATED).json({
-            success: true,
-            message: "User created successfully!",
-            data: user
-        })
+    res.status(httpStatus.CREATED).json({
+        success: true,
+        message: "User created successfully!",
+        data: user
+    })
+})
 
-    } catch (error: any) {
-        next(error);
-    }
-}
+// controller to create user - old way
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         const user = await UserServices.createUser(req.body);
+
+//         res.status(httpStatus.CREATED).json({
+//             success: true,
+//             message: "User created successfully!",
+//             data: user
+//         })
+
+//     } catch (error: any) {
+//         next(error);
+//     }
+// }
+
 
 // controller to get all users
-const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const allUsers = await UserServices.getAllUsers();
 
-        const allUsers = await UserServices.getAllUsers();
-
-        res.status(httpStatus.CREATED).json({
-            success: true,
-            message: "Got all users successfully!",
-            data: allUsers
-        })
-
-    } catch (error: any) {
-        next(error)
-    }
-}
+    res.status(httpStatus.OK).json({
+        success: true,
+        message: "Got all users successfully!",
+        data: allUsers
+    })
+})
 
 export const UserControllers = {
     createUser,
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,6 +4,6 @@ import { UserControllers } from "./user.controller";
 const router = Router();
 
 router.post('/register', UserControllers.createUser);
-router.get('/all', UserControllers.getAllUsers);
+router.get('/all-users', UserControllers.getAllUsers);
 
 export const UserRoutes = router;
