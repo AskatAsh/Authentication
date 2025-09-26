82c2457e6443b5c9015decd33ce060ae7bd5227a
AskatAsh <abdulaskat@gmail.com>
2025-09-26 12:55:02 +0600

    created route not found handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import notFoundHandler from './app/middlewares/notFoundHandler';
 import { router } from './app/routes';
 
 // create express app
@@ -24,5 +25,8 @@ app.get('/', (req: Request, res: Response) => {
 
 // global error handler
 app.use(globalErrorHandler);
+
+// not found handler
+app.use(notFoundHandler)
 
 export default app;
