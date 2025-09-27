025228b0de5d7ea8ba7b26c73cafdd9db65854b5
AskatAsh <abdulaskat@gmail.com>
2025-09-27 20:15:59 +0600

    used bcryptjs to to hash password

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import bcryptjs from "bcryptjs";
 import httpStatus from "http-status-codes";
 import AppError from "../../errorHelpers/appError";
 import { AuthProvider, IAuthProvider, IUser } from "./user.interface";
@@ -5,7 +6,7 @@ import User from "./user.model";
 
 // service function to create user
 const createUser = async (payload: Partial<IUser>) => {
-    const { email, ...rest } = payload;
+    const { email, password, ...rest } = payload;
 
     const isUserExist = await User.findOne({ email });
 
@@ -13,10 +14,15 @@ const createUser = async (payload: Partial<IUser>) => {
         throw new AppError(httpStatus.BAD_REQUEST, "This user already exists.");
     }
 
+    const salt = await bcryptjs.genSalt(10);
+
+    const hashedPassword = await bcryptjs.hash(password as string, salt);
+
     const authProvider: IAuthProvider = { provider: AuthProvider.CREDENTIAL, providerId: email as string };
 
     const user = await User.create({
         email,
+        password: hashedPassword,
         auths: [authProvider],
         ...rest
     });
