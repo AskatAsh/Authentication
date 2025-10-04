b9a544406c7ef9a6f1a15040d2bb0f4fbe2d68b4
AskatAsh <abdulaskat@gmail.com>
2025-10-04 23:08:32 +0600

    created error handler files for mongoose and zod errors

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,43 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from 'http-status-codes';
 import { envVars } from "../config/env";
 import AppError from "../errorHelpers/appError";
+import { handleCastError } from "../helpers/handleCastError";
+import { handleDupicateError } from "../helpers/handleDuplicateError";
+import { handleValidationError } from "../helpers/handleValidationError";
+import { handleZodError } from "../helpers/handleZodError";
+import { IErrorSources } from "../interfaces/error.types";
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
+// global error handler
 export const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
     let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     let message = "Something went wrong!";
 
-    const errorSources: any = [];
+    const errorSources: IErrorSources[] = [];
 
     // duplicate error
     if (error.code === 11000) {
-        statusCode = httpStatus.BAD_REQUEST;
-        const matchedArray = error.message.match(/"([^"]*)"/);
-        message = `${matchedArray[1]} Already Exists.`
+        const simplifiedError = handleDupicateError(error);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
     }
     // cast error / objectId validation error
     else if (error.name === "CastError") {
-        statusCode = httpStatus.BAD_REQUEST;
-        message = "Invalid MongoDB ObjectId. Please Provide a Valid ID."
+        const simplifiedError = handleCastError(error);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
     }
     // validation error
     else if (error.name === "ValidationError") {
-        const errors = Object.values(error.errors);
-
-        errors.forEach((errorObject: any) => errorSources.push({
-            path: errorObject.path,
-            message: errorObject.message
-        }));
-
-        statusCode = httpStatus.BAD_REQUEST;
-        message = "Validation Error Occured! Please Provide Valid Data."
+        const simplifiedError = handleValidationError(error, errorSources);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
     }
+    // zod error
+    else if (error.name === "ZodError") {
+        const simplifiedError = handleZodError(error, errorSources);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+    }
+    // errors from AppError
     else if (error instanceof AppError) {
         statusCode = error.statusCode;
         message = error.message;
-    } else if (error instanceof Error) {
+    }
+    // errors from Error
+    else if (error instanceof Error) {
         statusCode = httpStatus.INTERNAL_SERVER_ERROR;
         message = error.message;
     }
@@ -46,7 +56,7 @@ export const globalErrorHandler = (error: any, req: Request, res: Response, next
         success: false,
         message,
         errorSources,
-        error,
+        error: envVars.NODE_ENV === "development" ? error : null,
         stack: envVars.NODE_ENV === "development" ? error.stack : null
     })
 }
