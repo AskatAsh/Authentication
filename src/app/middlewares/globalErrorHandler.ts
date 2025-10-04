eb8e82c2a6c04a7279744029f987639c93d3a7f0
AskatAsh <abdulaskat@gmail.com>
2025-10-04 21:23:39 +0600

    handled mongoose duplicate cast and validation error in global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -9,7 +9,32 @@ export const globalErrorHandler = (error: any, req: Request, res: Response, next
     let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     let message = "Something went wrong!";
 
-    if (error instanceof AppError) {
+    const errorSources: any = [];
+
+    // duplicate error
+    if (error.code === 11000) {
+        statusCode = httpStatus.BAD_REQUEST;
+        const matchedArray = error.message.match(/"([^"]*)"/);
+        message = `${matchedArray[1]} Already Exists.`
+    }
+    // cast error / objectId validation error
+    else if (error.name === "CastError") {
+        statusCode = httpStatus.BAD_REQUEST;
+        message = "Invalid MongoDB ObjectId. Please Provide a Valid ID."
+    }
+    // validation error
+    else if (error.name === "ValidationError") {
+        const errors = Object.values(error.errors);
+
+        errors.forEach((errorObject: any) => errorSources.push({
+            path: errorObject.path,
+            message: errorObject.message
+        }));
+
+        statusCode = httpStatus.BAD_REQUEST;
+        message = "Validation Error Occured! Please Provide Valid Data."
+    }
+    else if (error instanceof AppError) {
         statusCode = error.statusCode;
         message = error.message;
     } else if (error instanceof Error) {
@@ -20,6 +45,7 @@ export const globalErrorHandler = (error: any, req: Request, res: Response, next
     res.status(statusCode).json({
         success: false,
         message,
+        errorSources,
         error,
         stack: envVars.NODE_ENV === "development" ? error.stack : null
     })
